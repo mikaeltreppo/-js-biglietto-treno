1b1d261a7295fbb328a1a1090f226286fb3434d7
mikaeltreppo <mikaeltreppo@outlook.com>
2023-02-22 14:18:19 +0100

    Fine esercizio

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 let eta = parseInt(prompt('Inserisci la tua età.'));
-console.log(eta);
+document.getElementById('eta').innerHTML = eta;
 
 let km = parseInt( prompt('inserisci i Km che vorresti percorrere'));
-console.log(km);
+document.getElementById('km').innerHTML = km;
 
 let costo = 0.21;
 let spesa = costo * km; 
@@ -14,13 +14,13 @@ let spesaJunior = spesa - scontoJunior;
 
 
 if (eta > 65){
-document.getElementById('costo').innerHTML = "Congratulazioni! Sei arrivato vivo fno a "+ eta +" anni, ti meriti uno sconto del 40%. Paghi solo: " + spesaSenior.toFixed(2) + "euro";
+document.getElementById('costo').innerHTML = "Congratulazioni! Sei arrivato vivo fino a "+ eta +" anni, ti meriti uno sconto del 40%. Paghi solo: " + spesaSenior.toFixed(2) + " euro";
 }
 else if (eta < 18){
-    document.getElementById('costo').innerHTML = "Ne spenderai di soldi quando sarai maggiorenne! Hai ancora " + eta + " anni, goditi uno sconto del 20%. Paghi solo: " + spesaJunior.toFixed(2) + "euro";
+    document.getElementById('costo').innerHTML = "Ne spenderai di soldi quando sarai maggiorenne! Hai ancora " + eta + " anni, goditi uno sconto del 20%. Paghi solo: " + spesaJunior.toFixed(2) + " euro";
 }
 else{
-    document.getElementById('costo').innerHTML = "Congratulazioni! Hai " + eta +" quindi nessuno ti vuole fare sconti! Paghi: " + spesa.toFixed(2) + "euro";
+    document.getElementById('costo').innerHTML = "Congratulazioni! Hai " + eta +" quindi nessuno ti vuole fare sconti! Paghi: " + spesa.toFixed(2) + " euro";
 }
 
 
